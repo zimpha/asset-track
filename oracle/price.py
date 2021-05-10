b0c277aff25a0712542c608c07415735c8c9f9ad
Xi Lin <zimpha@gmail.com>
2021-05-10 09:43:10 +0800

    feed some tokens' price with cmc

--- oracle/price.py
+++ oracle/price.py
@@ -8,11 +8,10 @@ cmc_slug_map = {
     'BTC': 'bitcoin',
     'CFX': 'conflux-network',
     'ETH': 'ethereum',
-    'USDT': 'tether',
-    'BUSD': 'binance-usd',
-    'DAI': 'multi-collateral-dai',
     'CAKE': 'pancakeswap',
     'BAKE': 'bakerytoken',
+    'BUNNY': 'pancakebunny',
+    'AUCTION': 'bounce-token',
 }
 
 def token_prices(token_slug_names):
@@ -21,14 +20,8 @@ def token_prices(token_slug_names):
     # FLUX Price
     response = requests.get('https://bsc.flux.01defi.com/api/v1/flux/get_trade_statistics_info').json()
     coin_cmc_price['FLUX'] = float(response['data']['fluxPrice'])
-    
-    # price from pancake
-    response = requests.get('https://api.pancakeswap.info/api/v2/tokens').json()
-    for _, token_info in response['data'].items():
-        symbol = token_info['symbol']
-        coin_cmc_price[symbol] = float(token_info['price'])
-    coin_cmc_price['BNB'] = coin_cmc_price['WBNB']
 
+    # price from CoinMarketCap
     cmc_base_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
     cmc_slug = list(cmc_slug_map.values())
     for slug in token_slug_names:
@@ -50,8 +43,15 @@ def token_prices(token_slug_names):
     for id in cmc_data:
         symbol = cmc_data[id]['symbol']
         price = cmc_data[id]['quote']['USD']['price']
+        coin_cmc_price[symbol] = price
+
+    # price from pancake
+    response = requests.get('https://api.pancakeswap.info/api/v2/tokens').json()
+    for _, token_info in response['data'].items():
+        symbol = token_info['symbol']
         if symbol not in coin_cmc_price:
-            coin_cmc_price[symbol] = price
+            coin_cmc_price[symbol] = float(token_info['price'])
+    coin_cmc_price['WBNB'] = coin_cmc_price['BNB']
     for stable_coin_name in ['USDT', 'BUSD', 'HUSD', 'DAI', 'USDC']:
         coin_cmc_price[stable_coin_name] = 1.0
     return coin_cmc_price
